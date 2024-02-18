41f3357e35dbc5417a2e6ff5d60f95bad0414568
Dede Iryawan <59034052+iryawan91@users.noreply.github.com>
2024-02-18 16:21:48 +0700

    Add files via upload

---
+++ IO.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int angkaBulat = 10;
+	float angkaKoma = 1.5f;
+	char karakter = 'a';
+	char string[100] = "Hello";
+	
+	
+	printf("%d\n", angkaBulat);
+	printf("%.2f\n", angkaKoma);
+	printf("%c\n", karakter);;
+	printf("%s\n", string);
+	return 0;
+}
