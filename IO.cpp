fadf695d548cc527876113fd177f6ba557ae2ad8
Dede Iryawan <59034052+iryawan91@users.noreply.github.com>
2024-02-19 05:44:27 +0700

    Add files via upload

--- IO.cpp
+++ IO.cpp
@@ -1,14 +1,28 @@
 #include<stdio.h>
 int main(){
-	int angkaBulat = 10;
-	float angkaKoma = 1.5f;
-	char karakter = 'a';
-	char string[100] = "Hello";
+	int angkaBulat;
+	float angkaKoma;
+	char karakter;
+	char string[100];
 	
-	
+	printf("Masukan Bilangan Bulat :");
+	scanf("%d", &angkaBulat);
+	getchar();
 	printf("%d\n", angkaBulat);
+	
+	printf("Masukan Bilangan Desimal :");
+	scanf("%f", &angkaKoma);
+	getchar();
 	printf("%.2f\n", angkaKoma);
-	printf("%c\n", karakter);;
+	
+	printf("Masukan Karkter :");
+	scanf("%c", &karakter);
+	getchar();
+	printf("%c\n", karakter);
+	
+	printf("Masukan Nama :");
+	scanf("%[^\n]", string);
+	getchar();
 	printf("%s\n", string);
 	return 0;
 }
